39ec99e24c51abb3e813465882989fdcd7933086
David Hoyt <dhoyt@hoyt.net>
2025-03-18 16:35:41 -0400

    Update IccJsonUtil Files
    Author: [David Hoyt](https://srd.cx/)
    
    Date: Tue Mar 18 1612 EDT 2025
    
    ```
    cd Build/
    export CXX=clang++
    cmake -DCMAKE_INSTALL_PREFIX="$HOME/.local" -DCMAKE_BUILD_TYPE=Debug  -DENABLE_TOOLS=ON -DENABLE_SHARED_LIBS=ON -DENABLE_STATIC_LIBS=ON -DENABLE_TESTS=ON -DENABLE_INSTALL_RIM=ON -DENABLE_ICCXML=ON -Wno-dev Cmake/
    make -j32
    ...
    [100%] Built target iccApplyProfiles
    ...
    Darwin Kernel Version 24.3.0: Thu Jan  2 20:22:00 PST 2025; root:xnu-11215.81.4~3/RELEASE_X86_64 x86_64
    [2025-03-18 14:17:54]
    ```
    
    ```
    cd Build/
    export CXX=g++
    cmake -DCMAKE_INSTALL_PREFIX=$HOME/.local -DCMAKE_BUILD_TYPE=Debug  -Wno-dev Cmake/ -DENABLE_TOOLS=ON -DENABLE_SHARED_LIBS=ON -DENABLE_STATIC_LIBS=ON -DENABLE_TESTS=ON -DENABLE_INSTALL_RIM=ON -DENABLE_ICCXML=ON -DCMAKE_RULE_MESSAGES=ON
    make -j32
    ...
    [100%] Built target iccApplyProfiles
    ...
    Darwin mini.local 24.3.0 Darwin Kernel Version 24.3.0: Thu Jan  2 20:22:00 PST 2025; root:xnu-11215.81.4~3/RELEASE_X86_64 x86_64
    [2025-03-18 14:28:19]
    ```
    
    ```
    cmake -S .  -B .  -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_TOOLCHAIN_FILE=C:/test/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_C_FLAGS="/MD /I C:/test/vcpkg/installed/x64-windows/include" -DCMAKE_CXX_FLAGS="/MD /I C:/test/vcpkg/installed/x64-windows/include" -DCMAKE_SHARED_LINKER_FLAGS="/LIBPATH:C:/test/vcpkg/installed/x64-windows/lib" -DENABLE_TOOLS=ON -DENABLE_SHARED_LIBS=ON -DENABLE_STATIC_LIBS=ON -DENABLE_TESTS=ON -DENABLE_INSTALL_RIM=ON -DENABLE_ICCXML=ON -DCMAKE_TOOLCHAIN_FILE=C:/test/vcpkg/scripts/buildsystems/vcpkg.cmake -DENABLE_TOOLS=ON -DENABLE_SHARED_LIBS=ON -DENABLE_STATIC_LIBS=ON -DENABLE_TESTS=ON -DENABLE_INSTALL_RIM=ON -DENABLE_ICCXML=ON
    cmake --build .  --config Debug  -- /m /maxcpucount:32
     1>Checking Build System
      IccProfLib2-static.vcxproj -> F:\xp\icc\Build\Cmake\IccProfLib\Debug\IccProfLib2-static.lib
      Auto build dll exports
      IccProfLib2.vcxproj -> F:\xp\icc\Build\Cmake\IccProfLib\Debug\IccProfLib2.dll
    ...
            Copying build artifact (exe) to Testing directory...
      Building Custom Rule F:/xp/icc/Tools/CmdLine/IccApplyNamedCmm/CMakeLists.txt
      iccApplyNamedCmm.vcxproj -> F:\xp\icc\Build\Cmake\Tools\IccApplyNamedCmm\Debug\iccApplyNamedCmm.exe
      Building Custom Rule F:/xp/icc/Build/Cmake/CMakeLists.txt
    ...
    Windows SDK version 10.0.22621.0 to target Windows 10.0.2610
    [2025-03-18 14:29:04 F:\xp\icc\Build\Cmake]
    ```

--- Tools/CmdLine/IccCommon/IccJsonUtil.h
+++ Tools/CmdLine/IccCommon/IccJsonUtil.h
@@ -1,42 +1,118 @@
+/*
+    File:       IccJsonUtil.h
+
+    Contains:   Utility function with working with nlohmann/json objects
+
+    Version:    V1
+
+    Copyright:  (c) see below
+*/
+
+/*
+ * The ICC Software License, Version 0.2
+ *
+ *
+ * Copyright (c) 2003-2024 The International Color Consortium. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. In the absence of prior written permission, the names "ICC" and "The
+ *    International Color Consortium" must not be used to imply that the
+ *    ICC organization endorses or promotes products derived from this
+ *    software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COLOR CONSORTIUM OR
+ * ITS CONTRIBUTING MEMBERS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the The International Color Consortium.
+ *
+ *
+ * Membership in the ICC is encouraged when this software is used for
+ * commercial purposes.
+ *
+ *
+ * For more information on The International Color Consortium, please
+ * see <http://www.color.org/>.
+ *
+ *
+ */
+
+ ////////////////////////////////////////////////////////////////////// 
+ // HISTORY:
+ //
+ // -Initial implementation by Max Derhak 1-11-2024
+ // -Added Explicit Template Instantiations D Hoyt 18-MAR-2025
+ //
+ //////////////////////////////////////////////////////////////////////
+
+#ifndef ICCJSONUTIL_H
+#define ICCJSONUTIL_H
+
 #include <list>
 #include <string>
-#include <nlohmann/json.hpp>
-
-#ifndef _ICCJSONUTIL_H
-#define _ICCJSONUTIL_H
-
+#include <vector>
 #include <nlohmann/json.hpp>
 #include "../../../IccProfLib/IccDefs.h"
 
-using namespace nlohmann;
+using json = nlohmann::json;
 
-template <class T>
-std::string arrayToJson(T *a, int nCount);
+// ========================
+// JSON Conversion Helpers
+// ========================
+template <typename T>
+std::string arrayToJson(T* a, int nCount);
 
-template<typename T>
+template <typename T>
 std::string valueToJson(const char* name, T v);
 
-template<>
+// Specialized `valueToJson` Templates
+template <>
 std::string valueToJson(const char* name, const char* v);
 
-template<>
+template <>
 std::string valueToJson(const char* name, char* v);
 
 std::string valueToJson(const char* name, const char* v, bool& bPreviousLine);
 
-template<>
+template <>
 std::string valueToJson(const char* name, float v);
 
-template<>
+template <>
 std::string valueToJson(const char* name, double v);
 
-template<>
-std::string valueToJson(const char* name, const std::string &);
+template <>
+std::string valueToJson(const char* name, const std::string& v);
 
-std::string valueToJsonUuid(const char* name, const char* v, bool bBinary=false);
-
+std::string valueToJsonUuid(const char* name, const char* v, bool bBinary = false);
 std::string fixJsonString(const char* v);
 
+// ========================
+// JSON Parsing Helpers
+// ========================
 template <typename T>
 bool jsonToValue(const json& j, T& nValue);
 
@@ -55,14 +131,21 @@ template <typename T>
 bool jsonToArray(const json& v, std::vector<T>& vals);
 
 bool jsonToArray(const json& v, std::vector<std::string>& vals);
-
 bool jsonToList(const json& v, std::list<std::string>& vals);
-
 bool jsonToCStr(const json& j, char* str, int nSize);
-
 bool jsonToColorSpace(const json& j, icColorSpaceSignature& sig);
 
-bool saveJsonAs(const json& j, const char* szFname, int indent=1);
+// ========================
+// JSON File I/O
+// ========================
+bool saveJsonAs(const json& j, const char* szFname, int indent = 1);
 bool loadJsonFrom(json& j, const char* szFname);
 
-#endif //_JSONUTIL
+// ========================
+// Prevent Duplicate Explicit Instantiation
+// ========================
+extern template bool jsonToValue<int>(const json&, int&);
+extern template bool jsonToValue<bool>(const json&, bool&);
+extern template bool jsonToValue<std::string>(const json&, std::string&);
+
+#endif  // ICCJSONUTIL_H
