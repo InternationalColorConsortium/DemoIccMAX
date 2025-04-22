c362345a252cc125c4f80d3610989e6a85460514
D Hoyt <dhoyt@hoyt.net>
2025-04-22 18:21:18 -0400

    Stage PR129
    Stage PR129

--- Testing/mcs/updateprevWithBkgd_d.bat
+++
@@ -1,3 +0,0 @@
-..\iccfromxml_d.exe 6ChanSelect-MID.xml 6ChanSelect-MID.icc
-..\iccfromxml_d.exe 18ChanWithSpots-MVIS.xml 18ChanWithSpots-MVIS.icc
-..\iccApplyProfiles_d.exe CMYKSS-Numbered-Overprint.tif prev.tif 1 0 1 0 1 6ChanSelect-MID.icc 0 -ENV:bkgX 0.4014 -ENV:bkgY 0.2391 -ENV:bkgZ 0.0272 18ChanWithSpots-MVIS.icc 0 ..\sRGB_IEC61966_21.icm 1
--- Testing/mcs/updateprev_d.bat
+++
@@ -1,3 +0,0 @@
-..\iccfromxml_d.exe 6ChanSelect-MID.xml 6ChanSelect-MID.icc
-..\iccfromxml_d.exe 18ChanWithSpots-MVIS.xml 18ChanWithSpots-MVIS.icc
-..\iccApplyProfiles_d.exe CMYKSS-Numbered-Overprint.tif prev.tif 1 0 1 0 1 6ChanSelect-MID.icc 0 18ChanWithSpots-MVIS.icc 0 ..\sRGB_IEC61966_21.icm 1
